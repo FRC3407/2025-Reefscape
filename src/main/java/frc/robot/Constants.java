223aeae738428b8415922d552c5f6206655eb534
binkus <104918629+centiweeger22@users.noreply.github.com>
2025-03-08 15:00:30 -0600

    more mores

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -81,7 +81,7 @@ public final class Constants {
   public static final class OIConstants {
     public static final int kDriverControllerPort = 2;
     public static final double kDriveDeadband = 0.05;
-    public static final double kSecondDriverPower = 0.5;  }
+    public static final double kSecondDriverPower = 0.15;  }
 
   public static final class AutoConstants {
     public static final double kMaxSpeedMetersPerSecond = 3;
