65d7c5d1f2dd6be40d257dcacfcd172f7c185e1d
QuantumKya <rohan.vats4nier@gmail.com>
2025-03-08 14:04:23 -0600

    Added configuracion for the coralator and flinger

--- src/main/java/frc/robot/subsystems/CorallatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/CorallatorSubsystem.java
@@ -1,60 +1,73 @@
 package frc.robot.subsystems;
 
+import org.opencv.video.SparseOpticalFlow;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkAnalogSensor;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
 
-public class CorallatorSubsystem extends SubsystemBase{
+public class CorallatorSubsystem extends SubsystemBase {
     private SparkMax m_wrist = new SparkMax(12, MotorType.kBrushless);
     private RelativeEncoder m_wristEncoder = m_wrist.getEncoder();
-    private SparkFlex m_flinger = new SparkFlex(13, MotorType.kBrushless);
+    private SparkFlex m_corallator = new SparkFlex(13, MotorType.kBrushless);
+    private SparkLimitSwitch m_limitSwitch = m_corallator.getForwardLimitSwitch();
     private final PIDController m_pidController = new PIDController(.02, 0, 0);
-    private final double targetAngleUpDegrees = 8;
+    private final double targetAngleUpDegrees = 8; // FIND THE RIGHT ANGLES!!!!!!! :3
     private final double targetAngleDownDegrees = -11;
     private double setPoint;
-    public double flingerSpeed = 0.15;
-    private boolean justDropped = false; 
-    private double dropTime = 0.0;
-     
-    public CorallatorSubsystem(){
-        // set_point = m_wristEncoder.getPosition();
+    public double flingerSpeed = 0.05;
+
+    public CorallatorSubsystem() {
+        m_wristEncoder.setPosition(0);
         setPoint = targetAngleDownDegrees;
-    }
-    public void angleUp(){
-        setPoint = targetAngleUpDegrees;
-    }
-    public void angleDown(){
-        setPoint = targetAngleDownDegrees;
-    }
-    public void intakeCoral(){
-        m_flinger.set(flingerSpeed);
-    }
-    public void outtakeCoral(){
-        m_flinger.set(-flingerSpeed);
-        dropTime = System.currentTimeMillis() * 1000;
+        m_wristEncoder.setPosition(0);
+        m_wrist.configure(Configs.CorallatorConfig.m_coralAngleator, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        m_corallator.configure(Configs.CorallatorConfig.m_coralFlingerator, ResetMode.kResetSafeParameters,
+        PersistMode.kPersistParameters);
     }
 
-    // FILL THIS WITH USEFUL STUFF WHEN L'ÉLECTRICIENNNNNNNNNN INSTALLS THE LIGHT THING
-    private boolean isTakingCoral() {
-        if (true) return false;
-        else return true;
+    public void angleUp() {
+        setPoint = targetAngleUpDegrees;
     }
+
+    public void angleDown() {
+        setPoint = targetAngleDownDegrees;
+    }
+
+    public void intakeCoral() {
+        m_corallator.set(flingerSpeed);
+    }
+
+    public void outtakeCoral() {
+        m_corallator.set(-flingerSpeed);
+    }
+
+    private boolean hasCoral() {
+        return m_limitSwitch.isPressed();
+    }
+
     public void stopCoral() {
-        m_flinger.set(0);
+        m_corallator.set(0);
     }
 
     @Override
-    public void periodic(){
+    public void periodic() {
         double wristAngle = m_wristEncoder.getPosition();
         double wristSpeed = m_pidController.calculate(m_wristEncoder.getPosition(), setPoint);
         SmartDashboard.putNumber("wrist encoder value", wristAngle);
         SmartDashboard.putNumber("wrist speed", wristSpeed);
-        //m_wrist.set(wristSpeed);
+
+        m_wrist.set(wristSpeed);
     }
 }
