b3255371c0678fe6730381b6cc225ab9cbcdb241
FRCuser <FRCuser@fakeemail.com>
2025-03-25 18:59:38 -0500

    changed correct level 3

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -109,7 +109,7 @@ public final class Constants {
     public static final double D_value = 0;
     public static final double level_1 = 0; //stopped here when adding variable for the height of each level
     public static final double level_2 = -5.0;
-    public static final double level_3 = -50.0;
+    public static final double level_3 = -35.0;
     public static final double level_4 = -70.0;
     public static final double d_stop = 0;
     public static final double coral_s = 0;
