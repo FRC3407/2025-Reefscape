cceaa6fa3e5f750e7b3bc0750260442ec2d8232f
binkus <104918629+centiweeger22@users.noreply.github.com>
2025-09-30 20:36:17 -0500

    climber go climber

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import static frc.robot.Constants.ClimberConstants.*;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+
+  private final SparkMax m_climber = new SparkMax(climberCanId, MotorType.kBrushless);
+  private final RelativeEncoder m_encoder = m_climber.getEncoder();
+  
+  private double targetPoint;
+  private boolean moving;
+  private int direction;
+  private int stage;
+  /** Creates a new ClimberSubsystem. */
+  public ClimberSubsystem() {
+    m_encoder.setPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Climber rotation:", m_encoder.getPosition());
+    SmartDashboard.putNumber("Climber stage:", stage);
+    SmartDashboard.putBoolean("Climber moving:", moving);
+    if (moving){
+      double targetDifference = m_encoder.getPosition()-targetPoint;
+      m_climber.set(Math.signum(targetDifference)*climberSpeed);
+      if (Math.signum(m_encoder.getPosition()-targetPoint) != direction){
+        moving = false;
+      }
+    }
+    else{
+      m_climber.set(0);
+    }
+
+    // This method will be called once per scheduler run
+  }
+
+  public void setPosition(double target) {
+    targetPoint = target;
+    moving = true;
+    direction = (int)Math.signum(m_encoder.getPosition()-targetPoint);
+  }
+
+  public void advanceClimber(int count){
+    stage+=count;
+    // if (stage < 0) stage = 2;
+    // if (stage > 2) stage = 0; //no need to loop over
+    setClimber();
+  }
+
+  public void climberForward(){
+    advanceClimber(1);
+  }
+
+  public void climberBackward(){
+    // advanceClimber(-1); //DONT go backwards!!!!!
+  }
+
+  public void setClimber(){
+    if (stage == 0){
+      setPosition(climberRestPosition);
+    }
+    if (stage == 1){
+      setPosition(climberHookPosition);
+    }
+    if (stage == 2){
+      setPosition(climberClimbedPosition);
+    }
+  }
+}
